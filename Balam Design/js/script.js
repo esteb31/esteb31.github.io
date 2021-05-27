bff5262a5f21a3010870b819710e3b53ae8e64fc
Esteban <aquir.eqs@gmail.com>
2021-05-27 12:48:08 -0600

    test 2.2

--- Balam Design/js/script.js
+++ Balam Design/js/script.js
@@ -1,4 +1,5 @@
 /*---------------- desplazamiento suave del one page ------------------*/
+
 $('a[href*=#]:not([href=#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
         var target = $(this.hash);
